c70449af0b91d7663f400b4f7e2a6c0a1f2e2509
Daveraj Chakravarty <drajchakravarty@hotmail.com>
2022-04-23 14:00:00 -0500

    New:

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 var fname = document.getElementById("fname").value;
 var lname = document.getElementById("lname").value;
 var saddy = document.getElementById("saddy").value;
-var city = document.getElementById("city").value;
-var state = document.getElementById("state").value;
+var c = document.getElementById("city").value;
+var s = document.getElementById("state").value;
 var zip = document.getElementById("zip").value;
 var can = document.getElementById("can").value;
-var Water = document.getElementById("Water").value;
+var W = document.getElementById("Water").value;
 var nonp = document.getElementById("nonp").value;
 var aid = document.getElementById("aid").value;
-var shirt = document.getElementById("shirt").value;
-var socks = document.getElementById("socks").value;
+var sh = document.getElementById("shirt").value;
+var scks = document.getElementById("socks").value;
 var tp = document.getElementById("tp").value;
-var battery = document.getElementById("battery").value;
+var bat = document.getElementById("battery").value;
+
+var holder = {};
+
+holder(zipcode) = zip;
+holder(canned) = can;
+holder(water) = W;
+holder(nonperish) = nonp;
+//holder(firstaid) = aid;
+//holder(shirt) = sh;
+//holder(sock) = scks;
+//holder(toilet) = tp;
+//holder(battery) = bat
+
+fetch("http://128.62.16.135", holder)
+
+
+
