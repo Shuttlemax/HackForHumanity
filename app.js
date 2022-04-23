d6df78451c71106d8372c28bf1b039e3dbaf06d3
Daveraj Chakravarty <drajchakravarty@hotmail.com>
2022-04-23 12:38:23 -0500

    fvfvds

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
-});
+var fname = document.getElementById("fname").value;
+var lname = document.getElementById("lname").value;
+var saddy = document.getElementById("saddy").value;
+var city = document.getElementById("city").value;
+var state = document.getElementById("state").value;
+var zip = document.getElementById("zip").value;
+var can = document.getElementById("can").value;
+var Water = document.getElementById("Water").value;
+var nonp = document.getElementById("nonp").value;
+var aid = document.getElementById("aid").value;
+var shirt = document.getElementById("shirt").value;
+var socks = document.getElementById("socks").value;
+var tp = document.getElementById("tp").value;
+var battery = document.getElementById("battery").value;
